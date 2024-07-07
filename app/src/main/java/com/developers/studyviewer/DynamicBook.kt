d27a882f811865cec4f0fa581455f260bf1bac29
UnknownStriker <136878011+UnknownStriker@users.noreply.github.com>
2024-07-07 12:21:59 +0900

    Update3

--- app/src/main/java/com/developers/studyviewer/DynamicBook.kt
+++ app/src/main/java/com/developers/studyviewer/DynamicBook.kt
@@ -1,30 +1,68 @@
 package com.developers.studyviewer
 
 import android.app.Activity
-import android.widget.Toast
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 
-
-
-class DynamicBook(private val activity: Activity) {
-    private var id = ""
-    private var cur = 0
-    private var origin = 0;
-    private var isFolding = false
+class DynamicBook(private val activity: Activity, private val librarian: Librarian) {
+    private var id = "" // Book ID
+    private var cur = 0 // Current page
+    private var origin = 0 // Originating shelf
+    private var maxPage = 0
+    private var isFolding = false // Is the book open
+    private val texts = arrayListOf<TextView>()
 
     private fun update() {
-        
-    }
-    private fun unload() {
+        if (texts.isNotEmpty()) {
 
+        }
     }
-    public fun loadBook(key: String, t: Int) {
-        if(isFolding) unload()
+
+    private fun unload() {
+        cur = 0
+        maxPage = 0
+        isFolding = false
+        id = ""
+        texts.clear()
+        librarian.loadShelf(origin)
+        origin = 0
+    }
+
+    fun loadBook(key: String, t: Int) {
         origin = t
         cur = 1
-        activity.setContentView(R.layout.bookopen)
         id = key
+        maxPage = 10
+        activity.setContentView(R.layout.bookopen)
+        setupNavigationButtons()
+        populateTextViews()
         update()
     }
 
+    private fun setupNavigationButtons() {
+        activity.findViewById<ImageButton>(R.id.next_button).setOnClickListener {
+            if (cur < maxPage) {
+                cur++
+                update()
+            }
+        }
+        activity.findViewById<ImageButton>(R.id.prev_button).setOnClickListener {
+            if (cur > 1) {
+                cur--
+                update()
+            }
+        }
+    }
 
+    private fun populateTextViews() {
+        val viewGroup = activity.findViewById<ConstraintLayout>(R.id.bookopen)
+        for (i in 0 until viewGroup.childCount) {
+            val child = viewGroup.getChildAt(i)
+            if (child is TextView) {
+                texts.add(child)
+            }
+        }
+    }
 }
--- app/src/main/java/com/developers/studyviewer/Librarian.kt
+++ app/src/main/java/com/developers/studyviewer/Librarian.kt
@@ -1,35 +1,34 @@
 package com.developers.studyviewer
+
 import android.app.Activity
-import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageButton
 import androidx.constraintlayout.widget.ConstraintLayout
 
-
 class Librarian(private val activity: Activity) {
-    private var book = DynamicBook(activity)
-    public fun loadShelf(num: Int) {
-        when(num) {
+    private val book = DynamicBook(activity, this)
+
+    fun loadShelf(num: Int) {
+        when (num) {
             1 -> {
                 activity.setContentView(R.layout.library_one)
-                val tmp: ViewGroup = activity.findViewById<ConstraintLayout>(R.id.library_one)
-                for (i in 0 until tmp.childCount) {
-                    val child = tmp.getChildAt(i)
-                    if(child is ImageButton) {
-                        child.setOnClickListener {
-                            book.loadBook(child.contentDescription.toString(), 1)
-                        }
-                    }
-                }
+                setupShelf(R.id.library_one, 1)
             }
-            //2 -> activity.setContentView(R.layout.library_two) 구현 안됨
-            //3 -> activity.setContentView(R.layout.library_three) 구현 안됨
-            //4 -> activity.setContentView(R.layout.library_four) 구현 안됨
-
         }
     }
 
-    public fun initialize() {
+    fun initialize() {
         loadShelf(1)
+    }
+
+    private fun setupShelf(layoutId: Int, shelfNumber: Int) {
+        val viewGroup = activity.findViewById<ConstraintLayout>(layoutId)
+        for (i in 0 until viewGroup.childCount) {
+            val child = viewGroup.getChildAt(i)
+            if (child is ImageButton) {
+                child.setOnClickListener {
+                    book.loadBook(child.contentDescription.toString(), shelfNumber)
+                }
+            }
+        }
     }
 }
